ea3987da6c638b658c1a4b907d28e6890c195a62
Yat Yee Li <twistedogic@gmail.com>
2024-11-07 23:19:02 +0800

    chore(nvim): setup telescope lsp key mapping

--- .config/nvim/lua/config/lazy/telescope.lua
+++ .config/nvim/lua/config/lazy/telescope.lua
@@ -7,7 +7,7 @@ return {
 	config = function()
 		local actions = require("telescope.actions")
 		local builtin = require("telescope.builtin")
-		local default_split = {
+		local default_mappings = {
 			n = {
 				["<CR>"] = actions.select_horizontal,
 			},
@@ -15,17 +15,33 @@ return {
 				["<CR>"] = actions.select_horizontal,
 			},
 		}
+		local default_insert = {
+			mappings = default_mappings,
+		}
+		local default_normal = {
+			initial_mode = "normal",
+			mappings = default_mappings,
+		}
 		require("telescope").setup({
 			defaults = {
 				layout_strategy = "vertical",
 			},
 			pickers = {
-				find_files = {
-					mappings = default_split,
-				},
+				find_files = default_insert,
+				live_grep = default_insert,
+				lsp_incoming_calls = default_normal,
+				lsp_outgoing_calls = default_normal,
+				lsp_references = default_normal,
+				lsp_definitions = default_normal,
+				diagnostics = default_normal,
 			},
 		})
 		vim.keymap.set("n", "<C-n>", builtin.find_files, {})
 		vim.keymap.set("n", "ff", builtin.live_grep, {})
+		vim.keymap.set("n", "gd", builtin.lsp_definitions, {})
+		vim.keymap.set("n", "<leader>ca", builtin.lsp_incoming_calls, {})
+		vim.keymap.set("n", "<leader>og", builtin.lsp_outgoing_calls, {})
+		vim.keymap.set("n", "<leader>rr", builtin.lsp_references, {})
+		vim.keymap.set("n", "<leader>ge", builtin.diagnostics, {})
 	end,
 }
