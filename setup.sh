1c87a3b860e39184c6e15319ec0ef7d149c8dee5
Jordan Li <twistedogic@gmail.com>
2026-04-06 00:18:41 +0800

    refactor: use mise

--- setup.sh
+++ setup.sh
@@ -1,61 +1,70 @@
 #!/bin/bash
+set -euo pipefail
+
 ############################
-# .make.sh
+# setup.sh
 # This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
 ############################
 
 ########## Variables
 
-dir=~/.my_dotfiles                    # dotfiles directory
-olddir=~/dotfiles_old             # old dotfiles backup directory
-files="bashrc config tmux.conf profile_alias config/nvim"    # list of files/folders to symlink in homedir
+dir="$HOME/.my_dotfiles"
+olddir="$HOME/dotfiles_old"
+files=("bashrc" "config" "tmux.conf" "profile_alias" "config/nvim")
 
 ##########
 
-git clone https://github.com/twistedogic/dotfiles $dir
+git clone https://github.com/twistedogic/dotfiles "$dir"
 
-if [ "$OSTYPE" == "linux-gnu"* ]; then
-    sudo apt update
-    sudo apt-get install --no-install-recommends -y nodejs npm neovim curl build-essential golang-go
-    curl --proto '=https' --tlsv1.3 https://sh.rustup.rs -sSf | sh -s -- -y
-elif [ "$OSTYPE" == "darwin"* ]; then
-    brew install neovim
-fi
+curl https://mise.run | sh
 
-# create dotfiles_old in homedir
+MISE_BIN="$HOME/.local/bin/mise"
+
+$MISE_BIN use -g node@latest
+$MISE_BIN use -g go@latest
+$MISE_BIN use -g uv@latest
+$MISE_BIN use -g neovim@0.11.6
+
 echo "Creating $olddir for backup of any existing dotfiles in ~"
-mkdir -p $olddir
+mkdir -p "$olddir"
 echo "...done"
 
-# change to the dotfiles directory
 echo "Changing to the $dir directory"
-cd $dir
+cd "$dir"
 echo "...done"
 
 backup() {
   local file=$1
-  if [ -f ".$file" ]; then
-    mv ~/.$file ~/dotfiles_old/
+  if [ -f ~/."$file" ]; then
+    mv ~/."$file" "$olddir"/
   fi
 }
 
-# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
-for file in $files; do
+for file in "${files[@]}"; do
   echo "Moving .$file from ~ to $olddir"
-  backup $file
+  backup "$file"
   echo "Copy .$file to ~"
-  cp -r $dir/.$file ~/.$file
+  cp -r "$dir/.$file" ~/."$file"
 done
 
-if [ -f ".zshenv" ]; then
-  backup zshenv 
+if [ -f ~/.zshenv ]; then
+  backup zshenv
   echo "source ~/.bashrc" >> ~/.zshenv
 fi
 
-if [ -f ".zshrc" ]; then
+if [ -f ~/.zshrc ]; then
   backup zshrc
   echo "source ~/.profile_alias" >> ~/.zshrc
   echo "source ~/.profile" >> ~/.zshrc
 fi
 
-rm -rf $dir
+case "$OSTYPE" in
+  linux-gnu*)
+    echo "eval \"\$(~/.local/bin/mise activate bash)\"" >> ~/.bashrc
+    ;;
+  darwin*)
+    echo "eval \"\$(~/.local/bin/mise activate zsh)\"" >> ~/.zshrc
+    ;;
+esac
+
+rm -rf "$dir"
